c5a688dc9878dd956408ba7bebe59652a601c22c
Yashveer Singh <yashveer100iit@gmail.com>
2026-06-08 22:32:27 +0530

    Implement checkInclusion method for string permutations

---
+++ Week_2/Day_2/Permutation in String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        if (n1 > n2) return false;
+        vector<int> freq1(26, 0), freq2(26, 0);
+        for (int i = 0; i < n1; i++) {
+            freq1[s1[i] - 'a']++;
+            freq2[s2[i] - 'a']++;
+        }
+        for (int i = n1; i < n2; i++) {
+            if (freq1 == freq2) return true;
+            freq2[s2[i] - 'a']++;          
+            freq2[s2[i - n1] - 'a']--;     
+        }
+        return freq1 == freq2;
+    }
+};
