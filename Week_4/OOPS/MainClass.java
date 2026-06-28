57d584d41b67f4b2ead948c50e981db8f465d517
Yashveer Singh <yashveer100iit@gmail.com>
2026-06-28 23:39:48 +0530

    Add MainClass with nested class example

---
+++ Week_4/OOPS/MainClass.java
@@ -0,0 +1,22 @@
+
+class Outer {
+    void display() {
+        System.out.println("Display of Outer class");
+    }
+
+    class Inner {
+        void display() {
+            System.out.println("Display of Inner class");
+        }
+    }
+}
+
+public class MainClass {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.display();
+
+        Outer.Inner inner = outer.new Inner();
+        inner.display();
+    }
+}
